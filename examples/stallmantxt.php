f88c33cda347d998ed8730e2716a861a3c0ecaf6
Merijn <mrbellek@gmail.com>
2014-12-29 16:09:13 +0100

    - finished richard stallman markovbot! - also updated founddildo filters

--- examples/stallmantxt.php
+++ examples/stallmantxt.php
@@ -1,9 +1,19 @@
 <?php
-require_once('../markovbot.php');
-require_once('./stallmantxt.inc.php');
+require_once('markovbot.php');
+require_once('stallmantxt.inc.php');
 
 $o = new MarkovBot(array(
-    'sUsername' => 'stallman_txt',
-    'sInputFile' => 'stallman.txt',
+    'sUsername'     => 'stallman_txt',
+    'sInputType'    => 'database',
+    'aDbSettings'   => array(
+        'sTable'        => 'stallman_txt',
+        'sIdCol'        => 'id',
+        'sCounterCol'   => 'postcount',
+        'sTimestampCol' => 'lasttimestamp',
+    ),
+    'sTweetFormat'  => ':tweet',
+    'aTweetVars'    => array(
+        array('sVar' => ':tweet', 'sRecordField' => 'tweet', 'bTruncate' => TRUE)
+    ),
 ));
 $o->run();
--- markovbot.php
+++ markovbot.php
@@ -3,13 +3,10 @@ require_once('twitteroauth.php');
 
 /*
  * TODO:
- * - input large body of text
- * - analyze, create markov chains
- * - construct tweets
- * - profit
- *
- * TODO:
- * - find a way to cache markov chains that loads faster than re-generating the markov chains every run
+ * - options to:
+ *   v generate markov chains on every run
+ *   v read pregenerated markov chains on every run
+ *   v read pregenerated messages from database
  */
 
 class MarkovBot {
@@ -38,8 +35,27 @@ class MarkovBot {
 
         //parse arguments, set values or defaults
         $this->sUsername        = (!empty($aArgs['sUsername'])       ? $aArgs['sUsername']      : '');
-        $this->sInputFile       = (!empty($aArgs['sInputFile'])      ? $aArgs['sInputFile']     : '');
+        $this->sInputType       = (!empty($aArgs['sInputType'])      ? $aArgs['sInputType']     : 'database');
         $this->sLogFile         = (!empty($aArgs['sLogFile'])        ? $aArgs['sLogFile']       : strtolower($this->sUsername) . '.log');
+
+        switch ($this->sInputType) {
+            case 'generate':
+                $this->sInputFile       = (!empty($aArgs['sInputFile'])      ? $aArgs['sInputFile']     : strtolower($this->sUsername) . '.csv');
+                break;
+
+            case 'pregenerated':
+                $this->sInputFile       = (!empty($aArgs['sInputFile'])      ? $aArgs['sInputFile']     : strtolower($this->sUsername) . '.json');
+                break;
+
+            case 'database':
+            default:
+                $this->aDbSettings      = (!empty($aArgs['aDbSettings'])     ? $aArgs['aDbSettings']        : array());
+                $this->aTweetSettings = array(
+                    'sFormat'       => (!empty($aArgs['sTweetFormat'])    ? $aArgs['sTweetFormat']   : ''),
+                    'aTweetVars'    => (!empty($aArgs['aTweetVars'])      ? $aArgs['aTweetVars']     : array()),
+                );
+                break;
+        }
 
         if ($this->sLogFile == '.log') {
             $this->sLogFile = pathinfo($_SERVER['SCRIPT_FILENAME'], PATHINFO_FILENAME) . '.log';
@@ -51,18 +67,58 @@ class MarkovBot {
         //get current user and verify it's the right one
         if ($this->getIdentity()) {
 
-            //analyze text into markov chain
-            if ($this->generateMarkovChain()) {
+            switch ($this->sInputType) {
 
-                //create a tweet!
-                if ($sTweet = $this->generateTweet()) {
+                //input text body, generate markov chains, generate tweet (least efficient)
+                case 'generate':
 
-                    //tweet it
-                    if ($this->postMessage($sTweet)) {
+                    //analyze text into markov chain
+                    if ($this->generateMarkovChain()) {
 
-                        $this->halt('Done!');
+                        //create a tweet!
+                        if ($sTweet = $this->generateTweet()) {
+
+                            //tweet it
+                            if ($this->postMessage($sTweet)) {
+
+                                $this->halt('Done!');
+                            }
+                        }
                     }
-                }
+                break;
+
+                //input markov chains, generate tweet
+                case 'pregenerated':
+
+                    //load previously generated markov chains
+                    if ($this->loadMarkovChain()) {
+
+                        //create a tweet!
+                        if ($sTweet = $this->generateTweet()) {
+
+                            //tweet it
+                            if ($this->postMessage($sTweet)) {
+
+                                $this->halt('Done!');
+                            }
+                        }
+                    }
+                break;
+
+                //read pregenerated tweet from database
+                case 'database':
+                default:
+
+                    //get a tweet!
+                    if ($sTweet = $this->getTweet()) {
+
+                        //tweet it
+                        if ($this->postMessage($sTweet)) {
+
+                            $this->halt('Done!');
+                        }
+                    }
+                break;
             }
         }
     }
@@ -128,6 +184,175 @@ class MarkovBot {
         return TRUE;
     }
 
+    private function loadMarkovChains() {
+
+        echo "Loading Markov chains\n";
+
+        if (is_file($this->sInputFile)) {
+
+            $lStart = microtime(TRUE);
+            $this->aMarkovChains = @json_decode(file_get_contents($this->sInputFile));
+
+            if ($iJsonErr = json_last_error()) {
+
+                printf("- error loading from %s: json_decode error %d\n\n", $this->sInputFile, $iJsonErr);
+                return FALSE;
+            } else {
+
+                printf("- done, loaded %d chains in %d seconds\n\n", count($this->aMarkovChains), microtime(TRUE) - $lStart);
+                return TRUE;
+            }
+        }
+
+        printf("- error loading from %s: file does not exist\n\n", $this->sInputFile);
+        return FALSE;
+    }
+
+    private function getTweet() {
+
+        if ($aRecord = $this->getRecord()) {
+
+            return $sTweet = $this->formatTweet($aRecord);
+        }
+    }
+
+    private function formatTweet($aRecord) {
+
+		//should get this by API (GET /help/configuration ->short_url_length) but it rarely changes
+		$iMaxTweetLength = 140;
+		$iShortUrlLength = 22;	//NB: 1 char more for https links
+
+		if (empty($this->aTweetSettings['sFormat']) || empty($this->aTweetSettings['aTweetVars'])) {
+			$this->logger(2, 'Tweet format settings missing.');
+			$this->halt('- One or more of the tweet format settings are missing, halting.');
+			return FALSE;
+		}
+
+		//construct tweet
+		$sTweet = $this->aTweetSettings['sFormat'];
+
+		//replace all non-truncated fields
+		foreach ($this->aTweetSettings['aTweetVars'] as $aVar) {
+			if (empty($aVar['bTruncate']) || $aVar['bTruncate'] == FALSE) {
+				$sTweet = str_replace($aVar['sVar'], $aRecord[$aVar['sRecordField']], $sTweet);
+			}
+		}
+
+		//determine maximum length left over for truncated field (links are shortened to t.co format of max 22 chars)
+		$sTempTweet = preg_replace('/http:\/\/\S+/', str_repeat('x', $iShortUrlLength), $sTweet);
+		$sTempTweet = preg_replace('/https:\/\/\S+/', str_repeat('x', $iShortUrlLength + 1), $sTempTweet);
+		$iTruncateLimit = $iMaxTweetLength - strlen($sTempTweet);
+
+		//replace truncated field
+		foreach ($this->aTweetSettings['aTweetVars'] as $aVar) {
+			if (!empty($aVar['bTruncate']) && $aVar['bTruncate'] == TRUE) {
+
+				//placeholder will get replaced, so add that to char limit
+				$iTruncateLimit += strlen($aVar['sVar']);
+
+				//get text to replace placeholder with
+				$sText = html_entity_decode($aRecord[$aVar['sRecordField']], ENT_QUOTES, 'UTF-8');
+
+				//get length of text with url shortening
+				$sTempText = preg_replace('/http:\/\/\S+/', str_repeat('x', $iShortUrlLength), $sText);
+				$sTempText = preg_replace('/https:\/\/\S+/', str_repeat('x', $iShortUrlLength + 1), $sTempText);
+				$iTextLength = strlen($sTempText);
+
+				//if text with url shortening falls under limit, keep it - otherwise truncate
+				if ($iTextLength <= $iTruncateLimit) {
+					$sTweet = str_replace($aVar['sVar'], $sText, $sTweet);
+				} else {
+					$sTweet = str_replace($aVar['sVar'], substr($sText, 0, $iTruncateLimit), $sTweet);
+				}
+
+				//only 1 truncated field allowed
+				break;
+			}
+		}
+
+		return $sTweet;
+    }
+
+    private function getRecord() {
+
+		echo "Getting random record from database..\n";
+
+		if (!defined('DB_HOST') || !defined('DB_NAME') ||
+			!defined('DB_USER') || !defined('DB_PASS')) {
+
+			$this->logger(2, 'MySQL database credentials missing.');
+			$this->halt('- One or more of the MySQL database credentials are missing, halting.');
+			return FALSE;
+		}
+
+        //connect to database
+		try {
+			$this->oPDO = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+		} catch(Exception $e) {
+			$this->logger(2, sprintf('Database connection failed. (%s)', $e->getMessage()));
+			$this->halt(sprintf('- Database connection failed. (%s)', $e->getMessage()));
+			return FALSE;
+		}
+
+		if (empty($this->aDbSettings) || empty($this->aDbSettings['sTable']) || empty($this->aDbSettings['sIdCol']) || 
+			empty($this->aDbSettings['sCounterCol']) || empty($this->aDbSettings['sTimestampCol'])) {
+
+			$this->logger(2, 'Database table settings missing.');
+			$this->halt('- One or more of the database table settings are missing, halting.');
+			return FALSE;
+		}
+
+        //fetch random record out of those with the lowest counter value
+        $sth = $this->oPDO->prepare(sprintf('
+            SELECT *
+            FROM %1$s
+            WHERE %2$s = (
+                SELECT MIN(%2$s)
+                FROM %1$s
+            )
+            ORDER BY RAND()
+            LIMIT 1',
+            $this->aDbSettings['sTable'],
+            $this->aDbSettings['sCounterCol']
+        ));
+
+		if ($sth->execute() == FALSE) {
+			$this->logger(2, sprintf('Select query failed. (%d %s)', $sth->errorCode(), $sth->errorInfo()));
+			$this->halt(sprintf('- Select query failed, halting. (%d %s)', $sth->errorCode(), $sth->errorInfo()));
+			return FALSE;
+		}
+
+        if ($aRecord = $sth->fetch(PDO::FETCH_ASSOC)) {
+            printf("- Found record that has been posted %d times before.\n", $aRecord['postcount']);
+
+            //update record with postcount and timestamp of last post
+            $sth = $this->oPDO->prepare(sprintf('
+                UPDATE %1$s
+                SET %3$s = %3$s + 1,
+                    %4$s = NOW()
+                WHERE %2$s = :id
+                LIMIT 1',
+                $this->aDbSettings['sTable'],
+                $this->aDbSettings['sIdCol'],
+                $this->aDbSettings['sCounterCol'],
+                $this->aDbSettings['sTimestampCol']
+            ));
+            $sth->bindValue(':id', $aRecord[$this->aDbSettings['sIdCol']], PDO::PARAM_INT);
+            if ($sth->execute() == FALSE) {
+                $this->logger(2, sprintf('Update query failed. (%d %s)', $stf->errorCode(), $sth->errorInfo()));
+                $this->halt(sprintf('- Update query failed, halting. (%d %s)', $sth->errorCode(), $sth->errorInfo()));
+                return FALSE;
+            }
+
+            return $aRecord;
+
+        } else {
+            $this->logger(3, 'Query yielded no results. (%d %s)', $sth->errorCode(), $sth->errorInfo());
+            $this->halt(sprintf('- Select query yielded no records, halting. (%d %s)', $sth->errorCode, $sth->errorInfo()));
+            return FALSE;
+        }
+    }
+
     private function generateTweet() {
 
         echo "Generating tweet..\n";
@@ -162,14 +387,13 @@ class MarkovBot {
     private function postMessage($sTweet) {
 
         //tweet
-        $oRet = TRUE;
-        //$oRet = $this->oTwitter->post('statuses/update', array('status' => $sTweet, 'trim_users' => TRUE));
+        $oRet = $this->oTwitter->post('statuses/update', array('status' => $sTweet, 'trim_users' => TRUE));
         if (isset($oRet->errors)) {
             $this->logger(2, sprintf('Twitter API call failed: statuses/update (%s)', $oRet->errors[0]->message));
             $this->halt('- Error: ' . $oRet->errors[0]->message . ' (code ' . $oRet->errors[0]->code . ')');
             return FALSE;
         } else {
-            printf("- %s\n", $sTweet);
+            printf("- Posted: %s\n", $sTweet);
         }
 
         return TRUE;
