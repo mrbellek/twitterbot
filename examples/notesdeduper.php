ad17110ec48f37fafdd2da56718a1300c7fca0e6
Merijn <mrbellek@gmail.com>
2015-01-21 10:59:40 +0100

    NotesFromBTC stuff: autogenerate insert query, moar filters

--- examples/notesdeduper.php
+++ examples/notesdeduper.php
@@ -16,6 +16,9 @@ if ($_POST) {
     foreach ($aNotes as $key => $sNote) {
         $sTransaction = substr(substr($sNote, strpos($sNote, '","') + 3), 0, -1);
         $sNoteText = substr($sNote, 1, strpos($sNote, '","') - 1);
+        if (substr($sNoteText, 0, 1) == '@') {
+            $sNoteText = '.' . $sNoteText;
+        }
 
         //check by transaction id and note text
         $sQuery = '
@@ -23,7 +26,7 @@ if ($_POST) {
             FROM notes
             WHERE tx = :tx
             OR note = :note
-            LIMIT 1'
+            LIMIT 1';
         $sth = $oPDO->prepare($sQuery);
         $sth->bindParam(':tx', $sTransaction, PDO::PARAM_STR);
         $sth->bindParam(':note', $sNoteText, PDO::PARAM_STR);
@@ -40,15 +43,28 @@ if ($_POST) {
     }
     printf('done, %d out of %d notes already in database</pre>' . "\r\n", $i, count($aNotes));
 
+    //convert the whole thing into a query for inserting into database
+    $aQuery = array('insert into notes(note, tx) values');
+    foreach ($aNotes as $sNote) {
+        if ($sNote) {
+            if (substr($sNote, 1, 1) == '@') {
+                $sNote = '".' . substr($sNote, 1);
+            }
+
+            $aQuery[] = '(' . $sNote . '),';
+        }
+    }
+    $aQuery[count($aQuery) - 1] = rtrim($aQuery[count($aQuery) - 1], ',') . ';';
 }
 ?>
 <html>
     <head><title>NotesFromBTC dupecheck</title></head>
     <body>
         <form method="post">
-        <textarea cols="200" rows="40" name="notes"><?= (!empty($aNotes) ? implode("\r\n", $aNotes) : '') ?></textarea>
+        <textarea cols="200" rows="40" name="notes"><?= (!empty($aNotes) ? htmlentities(implode("\r\n", $aNotes)) : '') ?></textarea>
             <br />
             <input type="submit" value="CHECK" />
         </form>
+        <pre><?= (!empty($aQuery) ? htmlentities(implode("\r\n", $aQuery)) : '') ?></pre>
     </body>
 </html>
