80bffa0c8439747e88f1a9398c36ad03a2427704
Merijn <mrbellek@gmail.com>
2015-03-18 17:03:41 +0100

    google2twitter changes: - delay popup added, click to cancel tweet - moved stuff around - next up: adding google analytics...

--- single-purpose/opera/content.js
+++ single-purpose/opera/content.js
@@ -2,12 +2,12 @@
  * TODO:
  * v abstract messaging to getOptions for clarity
  * v also hook image search etc, prefix searches with 'image search:' or something
- * - pretty up the options dialog (bootstrap.css?)
- * - figure out how to allow user to auth the app/extension instead of manually entering tokens?
- * - popup with twitter logo that stays for 2 seconds, then tweets - and cancels tweet when clicked
+ * v pretty up the options dialog (bootstrap.css?)
+ * v popup with twitter logo that stays for 2 seconds, then tweets - and cancels tweet when clicked
  *   - above delay configurable?
+ * . figure out how to allow user to auth the app/extension instead of manually entering tokens?
+ *   - broken in codebird.js :(
  * - package the whole thing, submit to Chrome Web Store as well as Opera Store
- * - oauth 2.0 twitter api library
  * - google analytics
  */
 
@@ -15,22 +15,90 @@ $(function() {
 
 	//search box is input[type="text"] with id 'lst-ib' for any search type
 
+	var iPopupTimer;
+
 	//trigger on page load (opera search keywords)
 	if ($('#lst-ib').length > 0) {
-		onSearch($('#lst-ib').val());
+		onBeforeSearch();
 	}
 
 	//catch when someone hits ENTER in search box
 	$('#lst-ib').keydown(function(e) {
 		if (e.which == 13) {
-			onSearch($('#lst-ib').val());
+			onBeforeSearch();
 		}
 	});
 
 	//catch when someone clicks the search button
 	$('button[name="btnG"]').click(function() {
-		onSearch($('#lst-ib').val());
+		onBeforeSearch();
 	});
+
+	function onBeforeSearch() {
+
+		console.log('showing twitter popup');
+		$('body').prepend('<link rel="stylesheet" href="' + chrome.extension.getURL('css/popup.css') + '" />');
+		$('body').append('<div id="popupDelay"><img src="' + chrome.extension.getURL('img/twitter.png') + '" width="48" height="48" alt="Click to cancel tweet" title="Click to cancel tweet" /></div>');
+		$('#popupDelay').animate({ bottom: 20 }, 'slow');
+		iPopupTimer = window.setTimeout(onSearch, 3500);
+	}
+
+	$('#popupDelay').click(function() {
+		console.log('tweet canceled.');
+		window.clearTimeout(iPopupTimer);
+		$('#popupDelay').animate({ bottom: -50 }, 'fast');
+	});
+
+	//main function for when a search is done
+	function onSearch() {
+		var tweet = $.trim($('#lst-ib').val());
+		if (tweet.length == 0) {
+			return;
+		}
+		$('#popupDelay').animate({ bottom: -50 }, 'fast');
+
+		//we're prefixing the search type if it's not a regular web search
+		type = getSearchType();
+		if (type != 'web') {
+			tweet = type + ' search: ' + tweet;
+		}
+
+		//set handler to catch message reply
+		chrome.runtime.onMessage.addListener(function(message) {
+
+			var consumer_key = message.consumer_key;
+			var consumer_secret = message.consumer_secret;
+			var access_key = message.access_key;
+			var access_secret = message.access_secret;
+
+			//basic check on tokens
+			if (validateTokens(consumer_key, consumer_secret, access_key, access_secret) == false) {
+				alert('Tokens not setup! Check console for errors.');
+				return;
+			}
+
+			console.log('tweetin: ' + tweet);
+			//return;
+
+			//tweet using CodeBird js library
+			cb = new Codebird;
+			cb.setConsumerKey(consumer_key, consumer_secret);
+			cb.setToken(access_key, access_secret);
+			cb.__call(
+				"statuses_update", { "status": tweet },
+				function(reply) {
+					if (typeof reply.errors != "undefined") {
+						console.log('twitter error: ' + reply.errors[0].message);
+					} else {
+						console.log('tweet posted.');
+					}
+				}
+			);
+		});
+
+		//send message to background script to fetch keys + secrets from settings
+		chrome.runtime.sendMessage('getOptions');
+	}
 
 	//fetch search type from 'tbm' query param
 	function getSearchType() {
@@ -69,56 +137,6 @@ $(function() {
 
 		//if nothing, it's regular web search
 		return 'web';
-	}
-
-	//main function for when a search is done
-	function onSearch(query) {
-		var tweet = $.trim(query);
-		if (tweet.length == 0) {
-			return;
-		}
-
-		//we're prefixing the search type if it's not a regular web search
-		type = getSearchType();
-		if (type != 'web') {
-			tweet = type + ' search: ' + tweet;
-		}
-
-		//set handler to catch message reply
-		chrome.runtime.onMessage.addListener(function(message) {
-
-			var consumer_key = message.consumer_key;
-			var consumer_secret = message.consumer_secret;
-			var access_key = message.access_key;
-			var access_secret = message.access_secret;
-
-			//basic check on tokens
-			if (validateTokens(consumer_key, consumer_secret, access_key, access_secret) == false) {
-				alert('Tokens not setup! Check console for errors.');
-				return;
-			}
-
-			console.log('tweetin: ' + tweet);
-			//return;
-
-			//tweet using CodeBird js library (Oauth 1.0)
-			cb = new Codebird;
-			cb.setConsumerKey(consumer_key, consumer_secret);
-			cb.setToken(access_key, access_secret);
-			cb.__call(
-				"statuses_update", { "status": tweet },
-				function(reply) {
-					if (typeof reply.errors != "undefined") {
-						console.log('twitter error: ' + reply.errors[0].message);
-					} else {
-						console.log('tweet posted.');
-					}
-				}
-			);
-		});
-
-		//send message to background script to fetch keys + secrets from settings
-		chrome.runtime.sendMessage('getOptions');
 	}
 
 	//basic token validation
