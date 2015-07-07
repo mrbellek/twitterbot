83071501803142d69e9d71bfa884333409ba92cd
Merijn <mrbellek@gmail.com>
2015-07-07 15:41:30 +0200

    put js back into header

--- single-purpose/google2twitter/options.js
+++ single-purpose/google2twitter/options.js
@@ -11,60 +11,6 @@ $(function() {
 		console.log('saved');
 	});
 
-	$('#auth').click(function(e) {
-		e.preventDefault();
-
-		chrome.storage.local.get([ 'consumer_key', 'consumer_secret' ], function(storage) {
-
-			var cb = new Codebird;
-			cb.setConsumerKey(storage.consumer_key, storage.consumer_secret);
-			cb.__call(
-				"oauth_requestToken",
-				{ oauth_callback: "oob" },
-				function (reply) {
-					// store it
-					cb.setToken(reply.oauth_token, reply.oauth_token_secret);
-
-					// gets the authorize screen URL
-					cb.__call(
-						"oauth_authorize",
-						{},
-						function (auth_url) {
-							window.codebird_auth = window.open(auth_url);
-						}
-					);
-				}
-			);
-		});
-	});
-
-	$('#verifypin').click(function(e) {
-		e.preventDefault();
-
-		chrome.storage.local.get([ 'consumer_key', 'consumer_secret' ], function(storage) {
-
-			var cb = new Codebird;
-			cb.setConsumerKey(storage.consumer_key, storage.consumer_secret);
-			alert($('#pinfield').val());
-			cb.__call(
-				"oauth_accessToken",
-				{ oauth_verifier: $('#pinfield').val() },
-				function(reply) {
-					alert('test');
-					//store the authenticated token, which may be different from the request token (!)
-					alert(reply.oauth_token, reply.oauth_token_secret);
-					cb.setToken(reply.oauth_token, reply.oauth_token_secret);
-
-					//persist access tokens
-					chrome.storage.local.set({
-						'access_token': reply.oauth_token,
-						'access_secret': reply.oauth_secret
-					});
-				}
-			);
-		});
-	});
-
 	function loadSettings() {
 
 		chrome.storage.local.get([ 'consumer_key', 'consumer_secret', 'access_key', 'access_secret' ], function(storage) {
