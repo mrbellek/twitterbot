40d8b4f53829ab82f2a09a5c76c652767cc2c92e
Merijn <merijn@mediablue.nl>
2016-08-23 12:04:22 +0200

    - fixed merging default settings with bot settings too soon - fixed setting of settings (oo is hard lol)

--- oo/lib/config.php
+++ oo/lib/config.php
@@ -9,15 +9,12 @@ class Config
 
         //load default settings
         if (is_file(MYPATH . 'default.json')) {
-            $this->oSettings = @json_decode(file_get_contents(MYPATH . 'default.json'));
+            $this->oDefaultSettings = @json_decode(file_get_contents(MYPATH . 'default.json'));
         }
 
         //load bot settings and merge
         if (is_file(MYPATH . strtolower($sUsername) . '.json')) {
-            $oBotSettings = @json_decode(file_get_contents(MYPATH . strtolower($sUsername) . '.json'));
-            if ($oBotSettings) {
-                $this->oSettings = (object) array_merge((array) $this->oSettings, (array) $oBotSettings);
-            }
+            $this->oSettings = @json_decode(file_get_contents(MYPATH . strtolower($sUsername) . '.json'));
         }
 
         return !is_null($this->oSettings);
@@ -25,22 +22,55 @@ class Config
 
     public function get($sName, $mDefault = false)
     {
-        return isset($this->oSettings->$sName) ? $this->oSettings->$sName : $mDefault;
+        if (isset($this->oSettings->$sName)) {
+
+            return $this->oSettings->$sName;
+
+        } elseif (isset($this->oDefaultSettings->$sName)) {
+
+            return $this->oDefaultSettings->$sName;
+
+        } else {
+
+            return $mDefault;
+        }
     }
 
     public function set()
     {
+        //get all func arguments
         $aArgs = func_get_args();
 
-        //TODO
+        //take out value
+        $mValue = array_pop($aArgs);
+
+        //take out property we want to set to above value
+        $oProp = array_pop($aArgs);
+
+        //recursively get node to set property of
+        //NB: no & operator needed since these are objects
+        $oNode = $this->oSettings;
+        foreach ($aArgs as $sSubnode) {
+            if (!isset($oNode->$sSubnode)) {
+                $oNode->$sSubnode = new stdClass;
+            }
+
+            $oNode = $oNode->$sSubnode;
+        }
+
+        //set property to new value
+        $oNode->$oProp = $mValue;
 
         //$this->writeConfig();
     }
 
     public function writeConfig()
     {
-        //TODO: fix this so it doesn't include default stuff
-
         file_put_contents(MYPATH . strtolower($this->sUsername) . '.json', json_encode($this->oSettings, JSON_PRETTY_PRINT));
+    }
+
+    public function __destruct()
+    {
+        //$this->writeConfig();
     }
 }
